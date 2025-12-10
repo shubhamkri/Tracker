c91519f56bf7b86e343856f9b45c117787ad11bb
shubhamkri <shubhamshukla89sk@gmail.com>
2025-12-10 13:52:11 +0530

    modify update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 resource "docker_container" "app" {
   name  = "my_app"
-  image = var.image       # uses the variable "image"
+  image = var.image
   ports {
     internal = var.port
     external = var.port
